018501b82d291afe782fef190af8e03a10a93515
Muhammedsarac-debug <muhammed.sarac@outlook.dk>
2025-10-20 21:25:13 +0100

    .

--- src/MariosPizza/Pizza.java
+++ src/MariosPizza/Pizza.java
@@ -1,4 +1,20 @@
 package MariosPizza;
 
-public class Pizza {
-}
+    // Enum navn
+    //Skal have en constructor
+
+    public Pizza(String name) {
+        // TODO: Sæt navn
+    }
+    public String ToString;
+
+    // TODO: ToString method
+
+    // TODO: Implementér getter for name
+
+// TODO: While loop og if statement
+
+// TODO: Getters og setters
+
+    // TODO: Implementér toString() for at vise pizzaens navn
+
