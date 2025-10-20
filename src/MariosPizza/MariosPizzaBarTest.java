ea5cb701c6cc42bbb3fdd2186b2402202c0d18e8
lukasma02 <andersenlukas02@gmail.com>
2025-10-20 14:45:31 +0200

    1stcommit mario

--- src/MariosPizza/MariosPizzaBarTest.java
+++ src/MariosPizza/MariosPizzaBarTest.java
@@ -1,4 +1,22 @@
 package MariosPizza;
 
 public class MariosPizzaBarTest {
+    // SETUP
+    // - 1. Test klasse skal kunne vise et display interface
+    // Opret ordre
+    // tjek id og status
+    // - 2. Beregning
+    // beregn og tjek pris er korrekt
+    // - 3. Sorter pizza kø
+    //tjek efter pizzaer med samme afhentningstid
+    //Hent sorteret liste
+    //Tjek liste er korrekt
+    // - 4. Skift status
+    //kald meteoder
+    //tjek status
+    //ændring af ordrer status
+    // - 5. færdiggøring af ordrer
+    //kalde relevante metoder til ændring og sletning af ordrer
+    // - 6. start program forfra
+
 }
