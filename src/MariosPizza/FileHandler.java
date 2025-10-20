78a399d05ef7015098e169bcd5c8a9580f6a3ea4
AlexArnold01 <alar1000@stud.ek.dk>
2025-10-20 15:30:54 +0200

    1. udkast af pseudo

--- src/MariosPizza/FileHandler.java
+++ src/MariosPizza/FileHandler.java
@@ -1,4 +1,28 @@
 package MariosPizza;
 
+import java.io.*;
+import java.util.List;
+
 public class FileHandler {
+
+
+    public void writeToFile(List<Pizza> orders){
+        //TODO - write to said file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("orders.csv"))) {
+             for(Pizza p : orders) {
+                writer.write(p.toString());
+                writer.newLine();
+            }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+    }
+
+    public readFromFile(){ //Læser fra filen, som printer ud i konsollen
+        //TODO - read from said file
+        //TODO - while loop
+        //TODO - try/catch method
+        //
+    }
 }
+
