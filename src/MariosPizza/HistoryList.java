fcf66e415580b2ef01ec0063949a20cf76468eb1
Frederick <frdu0001@stud@ek.dk>
2025-10-20 14:15:52 +0200

    First Psudo Code update. Frederick

--- src/MariosPizza/HistoryList.java
+++ src/MariosPizza/HistoryList.java
@@ -1,4 +1,19 @@
 package MariosPizza;
 
+import java.util.ArrayList;
+
 public class HistoryList {
+        public ArrayList<RemovedPizzas> RemovedPizzas = new ArrayList<>();
+        //TODO adds pizza which has been removed
+    }
+    public void RemovedPizzaList() {
+        //TODO pizza print out
+        //TODO i = size of pizzaList scales with pizza added.
+    }
+
+    public void WipePizzas() {
+        //TODO boolean for validering
+        //TODO while() loop med try catch,
+        //TODO clear()
+    }
 }
--- src/MariosPizza/PizzaList.java
+++ src/MariosPizza/PizzaList.java
@@ -1,4 +1,17 @@
 package MariosPizza;
 
+import java.util.ArrayList;
+
 public class PizzaList {
+    public ArrayList<Pizzas> Pizzas = new ArrayList<>();
+        //TODO add pizza
+    }
+    public void PizzaList() {
+        //TODO pizza print out
+        //TODO i = size of pizzaList scales with pizza added.
+    }
+
+    public void RemovedPizzas() {
+        //TODO boolean for validering
+        //TODO while loop med try catch, Pizza.remove
 }
