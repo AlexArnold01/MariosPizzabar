ae160a6a67a3437980182251a173a9f13d2a8f45
Frederick <frdu0001@stud@ek.dk>
2025-10-21 09:56:45 +0200

    PizzaList samll fix 01 Frederick.

--- src/MariosPizza/PizzaList.java
+++ src/MariosPizza/PizzaList.java
@@ -3,22 +3,22 @@ package MariosPizza;
 import java.util.ArrayList;
 
 public class PizzaList {
-    public ArrayList<Pizzas> Pizzas = new ArrayList<>();
-    //TODO add pizza
+    public ArrayList<Pizza> Pizzas = new ArrayList<>();
+
+    //adds Pizzas to the index
+    public void addPizza(Pizza pizza){
         Pizzas.add(pizza);
     }
+
+    //array list size
     public void PizzaList() {
-        //TODO pizza print out
-        //TODO i = size of pizzaList scales with pizza added.
         System.out.println("Pizzas:");
         for(int i=0; i < Pizzas.size(); i++) {
             System.out.println(i + 1 + ": " + Pizzas.get(i).toString());
         }
     }
-
+    //removes Pizza from list
     public void RemovedPizzas(int index) {
-        //TODO boolean for validering
-        //TODO while loop med try catch, Pizza.remove
         boolean valid = false;
         while(!valid) {
             try {
@@ -33,4 +33,5 @@ public class PizzaList {
                 System.out.printl("Error: Pizza in index" + index);
             }
         }
+    }
 }
