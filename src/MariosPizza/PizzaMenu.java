8993d2299eadd5ecec63de684740128a67de3785
Muhammedsarac-debug <muhammed.sarac@outlook.dk>
2025-10-21 10:30:10 +0100

    .

--- src/MariosPizza/PizzaMenu.java
+++ src/MariosPizza/PizzaMenu.java
@@ -1,24 +1,18 @@
 package MariosPizza;
 
 public enum PizzaMenu {
-    Vesuvio(57),
-    Amerikaner(53),
-    Cacciatore(57),
-    Carbona(63),
-    Dennis(65),
-    Bertil(57),
-    Silvia(61),
-    Victoria(61),
-    Toronfo(61),
-    Capricciosa(61),
-    Hawaii(61),
-    Blissola(61),
-    Venezia(61),
-    Mafia(61);
-
-    int Price;
-
-    MariosPizza (int Price){
-        this.Price = Price;
-    }
+    Vesuvio,
+    Amerikaner,
+    Cacciatore,
+    Carbona,
+    Dennis,
+    Bertil,
+    Silvia,
+    Victoria,
+    Toronfo,
+    Capricciosa,
+    Hawaii,
+    Blissola,
+    Venezia,
+    Mafia;
 }
