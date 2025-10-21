e0364b93c6a2a8e0687c42de95b78e7c118a1e80
Frederick <frdu0001@stud@ek.dk>
2025-10-21 09:50:27 +0200

    PizzaList er sat op men ikke færdigt Frederick.

--- src/MariosPizza/PizzaList.java
+++ src/MariosPizza/PizzaList.java
@@ -10,10 +10,27 @@ public class PizzaList {
     public void PizzaList() {
         //TODO pizza print out
         //TODO i = size of pizzaList scales with pizza added.
-        System.out.println()
+        System.out.println("Pizzas:");
+        for(int i=0; i < Pizzas.size(); i++) {
+            System.out.println(i + 1 + ": " + Pizzas.get(i).toString());
+        }
     }
 
-    public void RemovedPizzas() {
+    public void RemovedPizzas(int index) {
         //TODO boolean for validering
         //TODO while loop med try catch, Pizza.remove
+        boolean valid = false;
+        while(!valid) {
+            try {
+                Pizza pizza = Pizzas.get(index);
+                String name = pizza.GetName();
+
+                valid = true;
+                Pizzas.remove(index);
+                System.out.println("Pizza removed: " + name);
+
+            } catch (IndexOutOfBoundsException e) {
+                System.out.printl("Error: Pizza in index" + index);
+            }
+        }
 }
