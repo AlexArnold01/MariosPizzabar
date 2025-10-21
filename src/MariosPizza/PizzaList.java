ae897f6137283ef1af583f83b97b49453b6e4582
Frederick <frdu0001@stud@ek.dk>
2025-10-21 09:15:12 +0200

    added print statment, add pizza til pizzaList. Frederick.

--- src/MariosPizza/PizzaList.java
+++ src/MariosPizza/PizzaList.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 
 public class PizzaList {
     public ArrayList<Pizzas> Pizzas = new ArrayList<>();
-        //TODO add pizza
+    //TODO add pizza
+        Pizzas.add(pizza);
     }
     public void PizzaList() {
         //TODO pizza print out
         //TODO i = size of pizzaList scales with pizza added.
+        System.out.println()
     }
 
     public void RemovedPizzas() {
