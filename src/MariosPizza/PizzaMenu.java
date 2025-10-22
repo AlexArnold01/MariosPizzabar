807cb5aa12686669a7b7271f487bb00e7ded1f00
Muhammedsarac-debug <muhammed.sarac@outlook.dk>
2025-10-22 10:24:42 +0100

    .

--- src/MariosPizza/PizzaMenu.java
+++ src/MariosPizza/PizzaMenu.java
@@ -1,18 +1,24 @@
 package MariosPizza;
 
 public enum PizzaMenu {
-    Vesuvio,
-    Amerikaner,
-    Cacciatore,
-    Carbona,
-    Dennis,
-    Bertil,
-    Silvia,
-    Victoria,
-    Toronfo,
-    Capricciosa,
-    Hawaii,
-    Blissola,
-    Venezia,
-    Mafia;
+    Vesuvio(1),
+    Amerikaner(2),
+    Cacciatore(3),
+    Carbona(4),
+    Dennis(5),
+    Bertil(6),
+    Silvia(7),
+    Victoria(8),
+    Toronfo(9),
+    Capricciosa(10),
+    Hawaii(11),
+    Blissola(12),
+    Venezia(13),
+    Mafia(14);
+
+    int Number;
+
+    PizzaMenu (int Number){
+        this.Number = Number;
+    }
 }
